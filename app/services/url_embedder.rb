5aac01c22310a7e7ad7f43531c6157d8318efc1f
Martin-Louis Bright <mlbright@gmail.com>
2025-11-25 22:13:01 -0500

    allow youtube time parameters

--- app/services/url_embedder.rb
+++ app/services/url_embedder.rb
@@ -42,9 +42,12 @@ class UrlEmbedder
       video_id = extract_youtube_id(url, uri)
       return embed_link(url) unless video_id
 
+      # Extract time parameters (t= or start=) from the original URL
+      time_param = extract_youtube_time_param(uri)
+
       <<~HTML.html_safe
         <div class="embed-container">
-          <iframe src="https://www.youtube-nocookie.com/embed/#{video_id}"#{' '}
+          <iframe src="https://www.youtube-nocookie.com/embed/#{video_id}#{time_param}"#{' '}
                   frameborder="0"#{' '}
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"#{' '}
                   allowfullscreen>
@@ -52,12 +55,15 @@ class UrlEmbedder
         </div>
       HTML
     elsif host.include?("youtu.be")
-      video_id = uri.path.split("/").last
+      video_id = uri.path.split("/").last&.split("?")&.first
       return embed_link(url) unless video_id
+
+      # Extract time parameters from youtu.be URLs
+      time_param = extract_youtube_time_param(uri)
 
       <<~HTML.html_safe
         <div class="embed-container">
-          <iframe src="https://www.youtube-nocookie.com/embed/#{video_id}"#{' '}
+          <iframe src="https://www.youtube-nocookie.com/embed/#{video_id}#{time_param}"#{' '}
                   frameborder="0"#{' '}
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"#{' '}
                   allowfullscreen>
@@ -93,6 +99,56 @@ class UrlEmbedder
     end
 
     nil
+  end
+
+  def self.extract_youtube_time_param(uri)
+    return "" unless uri.query
+
+    params = URI.decode_www_form(uri.query).to_h
+
+    # Handle both 't' parameter (from share links) and 'start' parameter (embed links)
+    time_value = params["t"] || params["start"]
+    return "" unless time_value
+
+    # Sanitize: ensure it's only digits and optional 's' suffix
+    # YouTube accepts formats like: 123, 123s, 1m30s, 1h2m3s
+    if time_value.match?(/^[\dsmh]+$/)
+      # Convert to seconds if needed (YouTube embed only accepts 'start' in seconds)
+      seconds = parse_youtube_time(time_value)
+      "?start=#{seconds}" if seconds
+    else
+      ""
+    end
+  end
+
+  def self.parse_youtube_time(time_str)
+    return nil if time_str.blank?
+
+    # Remove trailing 's' if present
+    time_str = time_str.gsub(/s$/, "")
+
+    # If it's just a number, return it
+    return time_str.to_i if time_str.match?(/^\d+$/)
+
+    # Parse time formats like "1h2m3" or "2m30"
+    hours = 0
+    minutes = 0
+    seconds = 0
+
+    if time_str =~ /(\d+)h/
+      hours = $1.to_i
+    end
+
+    if time_str =~ /(\d+)m/
+      minutes = $1.to_i
+    end
+
+    # Get remaining seconds after h and m
+    remaining = time_str.gsub(/\d+[hm]/, "")
+    seconds = remaining.to_i if remaining.match?(/^\d+$/)
+
+    total_seconds = (hours * 3600) + (minutes * 60) + seconds
+    total_seconds > 0 ? total_seconds : nil
   end
 
   def self.embed_image(url)
